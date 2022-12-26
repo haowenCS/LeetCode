5cc7d6141fdf71b2053fe5874c5e3d3e7c44f4b7
haowencs <haowen_cs@hotmail.com>
2022-12-26 13:11:20 +0800

    Leetcode: Valid Parentheses, python [stack:list]

---
+++ 019. Valid Parentheses/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closeToOpen = {")": "(", "]": "[", "}": "{"}
+
+        for c in s:
+            if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+
+        return True if not stack else False
+
+if __name__ == "__main__":
+    s = "()[]{}"
+    print(Solution().isValid(s))
+    # assert (Solution().twoSum(nums, target) == [0, 1])
