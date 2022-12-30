e72bb68199c20aabaeb9d0245a97834d18fa4b6f
haowencs <haowen_cs@hotmail.com>
2022-12-30 12:25:05 +0800

    Leetcode: Single number, python [^ xor operator]

--- 135. Single Number/solution.py
+++ 135. Single Number/solution.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def singleNumber(self, nums):
         """
@@ -8,6 +11,15 @@ class Solution:
         for num in nums:
             single ^= num
         return single
+
+
+class Solution2:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0  # n ^ 0 = n
+        for n in nums:
+            res = n ^ res
+
+        return res
 
 
 # test
