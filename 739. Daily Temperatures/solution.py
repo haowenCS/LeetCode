c45884e5d9e3481d1b38ee048a950467c836785e
haowencs <haowen_cs@hotmail.com>
2023-04-02 11:47:23 +0800

    stack, python

---
+++ 739. Daily Temperatures/solution.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0] * len(temperatures)
+        stack = []  # pair: [temp, index]
+        # 用空间换时间，否则暴力求解O(n2)
+
+        for i, t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                stackT, stackInd = stack.pop()
+                res[stackInd] = (i - stackInd)
+            stack.append([t, i])
+
+        return res
